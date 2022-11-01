699d5628dbd6014b698f64d69f5ed5b9eeebf772
nkozlo3 <80439868+nkozlo3@users.noreply.github.com>
2022-11-01 18:09:55 -0500

    Started graph.cpp

---
+++ src/graph.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file graph.cpp
+ * Final Project.
+ */
+#include "graph.h"
+
+
+/**
+This function will convert a csv to a vector of vector of strings. This will make it easier to work with the data. 
+*/
+std::vector<std::vector<std::string>> csvToVect(std::string filename)
+{
+    //@TODO:
+    std::vector<std::vector<std::string>> v;
+
+    return v;
+}
+
