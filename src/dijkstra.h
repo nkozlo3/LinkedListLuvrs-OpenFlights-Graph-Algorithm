b9df2fa422ad049774a286e3aab9c2cc87fd6c7f
nisharamananathan <78706007+nisharamananathan@users.noreply.github.com>
2022-11-13 16:23:42 -0600

    updates

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -7,10 +7,7 @@ class Dijkstras
 public:
     Dijkstras();      // TODO:
     Graph getGraph(); // TODO:
-<<<<<<< HEAD
     int minDist(std::vector<int> distances, std::vector<bool> visited);
     void dijkstra(std::vector<std::vector<int>> graph, int startVertex);
-=======
->>>>>>> parent of 9f196b8 (.)
 private:
 };
