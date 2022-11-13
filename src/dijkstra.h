b7eb3ddb5df83ab7f38fe6b3f3f8be4c6bb1e2df
nisharamananathan <78706007+nisharamananathan@users.noreply.github.com>
2022-11-13 15:40:32 -0600

    .

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -7,7 +7,5 @@ class Dijkstras
 public:
     Dijkstras();      // TODO:
     Graph getGraph(); // TODO:
-    int minDist(std::vector<int> distances, std::vector<bool> visited);
-    void dijkstra(std::vector<std::vector<int>> graph, int startVertex);
 private:
 };
