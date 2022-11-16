82ba9b1befa56231af206f14e209838e083d3b44
nisharamananathan <78706007+nisharamananathan@users.noreply.github.com>
2022-11-16 15:28:06 -0600

    updated dfs.cpp

--- src/dfs.h
+++ src/dfs.h
@@ -1,15 +1,15 @@
-// #pragma once
+#pragma once
 
-// #include <iostream>
-// #include "graph.h"
-// class DFS : public Graph
-// {
-// public:
-//     void dfs();                          // TODO:
-//     void dfs(std::string airport);  // TODO:
-//     ~DFS();                         // TODO:
-//     std::vector<std::string> flight_path;
-//     std::set<std::string> visited;
+#include <iostream>
+#include "graph.h"
+class DFS : public Graph
+{
+public:
+    void dfs();                          // TODO:
+    void dfs(std::string airport);  // TODO:
+    ~DFS();                         // TODO:
+    std::vector<std::string> flight_path;
+    std::set<std::string> visited;
 
-// private:
-// };
+private:
+};
