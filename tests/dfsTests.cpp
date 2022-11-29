5eb386df0685236640ce59727ca54e19d582af65
nisharamananathan <78706007+nisharamananathan@users.noreply.github.com>
2022-11-29 07:15:16 -0600

    Update dfsTests.cpp

---
+++ tests/dfsTests.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "graph.h"
+#include "dfs.h"
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+Graph g(1, 1, "mercator🤠.png");
+TEST_CASE("DFS returns correct traversal", "[weight=1][dfs][11]") {
+    
+}
